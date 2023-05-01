be175518010458fae870b1d89a0013a88ad40293
Kihun SONG <kihunbuzz@naver.com>
2023-05-01 16:44:29 +0900

    [FE] Style : Color, Font Add

--- client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/ui/theme/Color.kt
+++ client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/ui/theme/Color.kt
@@ -6,3 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Purple80 = Color(0xFFD0BCFF)
+val BottomSheetColor = Color(0xFFF7F2F9)
+val iconColor = Color(0xFF6750A4)
