865bd8d6ce5f8f31d750f486ff9abe4bffa2f654
Kihun SONG <kihunbuzz@naver.com>
2023-05-02 15:07:28 +0900

    [FE] Feat : ZoomableImage

---
+++ client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/components/ZoomableImage.kt
@@ -0,0 +1,106 @@
+package com.dijkstra.pathfinder.components
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.dijkstra.pathfinder.ui.theme.IconColor
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.roundToInt
+import kotlin.math.sin
+
+@ExperimentalFoundationApi
+@Composable
+fun ZoomableImage(
+    model: Any,
+    contentDescription: String? = null,
+) {
+    var angle by remember { mutableStateOf(0f) }
+    var zoom by remember { mutableStateOf(1f) }
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+
+    val animatedZoom by animateFloatAsState(
+        targetValue = zoom,
+    )
+
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp.value
+    val screenHeight = configuration.screenHeightDp.dp.value
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Transparent)
+            .combinedClickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = {},
+                onDoubleClick = {
+                    if (zoom != 1f) {
+                        zoom = 1f
+                        angle = 0f
+                        offsetX = 0f
+                        offsetY = 0f
+                    } else if (angle == 0f && offsetX == 0f && offsetY == 0f) {
+                        zoom = 2f
+                    }
+                }
+            )
+    ) {
+        AsyncImage(
+            model,
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .graphicsLayer(
+                    scaleX = animatedZoom,
+                    scaleY = animatedZoom,
+                    rotationZ = angle
+                )
+                .pointerInput(Unit) {
+                    detectTransformGestures(
+                        onGesture = { centroid, pan, gestureZoom, gestureRotate ->
+                            angle += gestureRotate
+                            zoom = (zoom * gestureZoom).coerceIn(1F..3F)
+                            if (zoom > 1) {
+                                val x = (pan.x * zoom)
+                                val y = (pan.y * zoom)
+                                val angleRad = angle * PI / 180.0
+
+                                offsetX =
+                                    (offsetX + (x * cos(angleRad) - y * sin(angleRad)).toFloat()).coerceIn(
+                                        -(screenWidth * zoom)..(screenWidth * zoom)
+                                    )
+                                offsetY =
+                                    (offsetY + (x * sin(angleRad) + y * cos(angleRad)).toFloat()).coerceIn(
+                                        -(screenHeight * zoom)..(screenHeight * zoom)
+                                    )
+                            } else {
+                                offsetX = 0F
+                                offsetY = 0F
+                            }
+                        }
+                    )
+                }
+                .fillMaxSize()
+        )
+    }
+}
