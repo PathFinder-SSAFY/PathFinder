6df302c5ca85db4271c2306e43edfe3ab09ee88d
Kihun SONG <kihunbuzz@naver.com>
2023-05-02 17:58:47 +0900

    [FE] Fix : Unused Import

--- client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/MainActivity.kt
+++ client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.dijkstra.pathfinder.navigation.AppNavigation
 import com.dijkstra.pathfinder.screen.main.MainScreen
 import com.dijkstra.pathfinder.ui.theme.PathFinderTheme
 import dagger.hilt.android.AndroidEntryPoint
