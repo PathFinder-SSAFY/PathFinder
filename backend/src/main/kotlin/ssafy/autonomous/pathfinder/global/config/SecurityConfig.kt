8b9d2425020e65b7fc7341dc5914ce9b23854b1c
lkc263 <lkc263@naver.com>
2023-05-03 17:50:15 +0900

    [BE] Feat : Cookie, SuccessHandler (in naver oauth login)

--- backend/src/main/kotlin/ssafy/autonomous/pathfinder/global/config/SecurityConfig.kt
+++ backend/src/main/kotlin/ssafy/autonomous/pathfinder/global/config/SecurityConfig.kt
@@ -13,10 +13,11 @@ import ssafy.autonomous.pathfinder.domain.auth.service.CustomUserDetailsService
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
-    private val tokenAuthenticationFilter: TokenAuthenticationFilter,
-    private val customUserDetailsService: CustomUserDetailsService,
-    private val customOAuth2UserService: CustomOAuth2UserService,
-    // 성공, 실패, Cookie 처리까지
+        private val tokenAuthenticationFilter: TokenAuthenticationFilter,
+        private val customUserDetailsService: CustomUserDetailsService,
+        private val customOAuth2UserService: CustomOAuth2UserService, // 성공, 실패, Cookie 처리까지
+        private val
+
 
 //        private val oAuth2UserService: AdministratorOAuth2UserServiceImpl,
 //        private val authenticationSuccessHandler: OAuth2AuthenticationSuccessHandler,
@@ -37,7 +38,6 @@ class SecurityConfig(
 //                .failureHandler(authenticationoAuth2FailureHandler)
 //        http.addFilterBefore(token)
     }
-
 
 
 }
