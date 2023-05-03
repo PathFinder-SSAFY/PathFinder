f315bf82a11fd3e519543bae786016a408b87f8f
고청천 <g2c1010@naver.com>
2023-05-03 16:29:30 +0900

    [BE] Feat: db 설정  파일

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 //	testImplementation("org.springframework.security:spring-security-test")
 
 	// S3
-	implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+//	implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
 }
 
 tasks.withType<KotlinCompile> {
