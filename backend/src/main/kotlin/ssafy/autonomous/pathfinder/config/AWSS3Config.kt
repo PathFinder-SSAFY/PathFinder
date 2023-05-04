c3ba8d21bea00b63bdd4189d526a1feb0756e1a3
고청천 <g2c1010@naver.com>
2023-05-04 15:01:30 +0900

    [BE] Feat: S3 Config 작성

--- backend/src/main/kotlin/ssafy/autonomous/pathfinder/config/AWSS3Config.kt
+++ backend/src/main/kotlin/ssafy/autonomous/pathfinder/config/AWSS3Config.kt
@@ -1,28 +1,35 @@
-//package ssafy.autonomous.pathfinder.config
-//
-//import org.springframework.beans.factory.annotation.Value
-//import org.springframework.context.annotation.Bean
+package ssafy.autonomous.pathfinder.config
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 //import org.springframework.context.annotation.Configuration
 //import org.springframework.context.annotation.Primary
 //
 //@Configuration
-//class AWSS3Config {
-//    @Value("\${cloud.aws.credentials.access-key}")
-//    private lateinit var accssKey: String;
-//
-//    @Value("\${cloud.aws.credentials.secret-key}")
-//    private lateinit var secretKey: String
-//
-//    @Value("\${cloud.aws.region.static}")
-//    private lateinit var region: String
-//
-//    @Primary
-//    @Bean
-//    fun amazonS3Client(): Amazon? {
-//        val awsCredentials = BasicAWSCredentials(accessKey, secretKey)
-//        return AmazonS3ClientBuilder.standard()
-//                .withRegion(region)
-//                .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
-//                .build() as AmazonS3Client
-//    }
-//}
+class AWSS3Config {
+    @Value("\${cloud.aws.credentials.access-key}")
+    private lateinit var accssKey: String;
+
+    @Value("\${cloud.aws.credentials.secret-key}")
+    private lateinit var secretKey: String
+
+    @Value("\${cloud.aws.region.static}")
+    private lateinit var region: String
+
+    @Bean
+    fun BasicAWSCredentials(): BasicAWSCredentials {
+        return BasicAWSCredentials(accssKey, secretKey)
+    }
+
+    @Bean
+    fun amazonS3(basicAWSCredentials: BasicAWSCredentials): AmazonS3 {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build()
+    }
+}
