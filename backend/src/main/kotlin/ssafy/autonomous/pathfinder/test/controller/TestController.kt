d59ce52c60a67b6b4a37904dcc33dc508ab126d1
고청천 <g2c1010@naver.com>
2023-05-04 13:02:24 +0900

    [BE] Fix: 테스트 Api http 메서드 수정 post -> get

--- backend/src/main/kotlin/ssafy/autonomous/pathfinder/test/controller/TestController.kt
+++ backend/src/main/kotlin/ssafy/autonomous/pathfinder/test/controller/TestController.kt
@@ -4,13 +4,14 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 
 @Api(tags=["통신 테스트 컨트롤러"])
 @Controller
 class TestController {
-    
-    @PostMapping("/hello")
+
+    @GetMapping("/hello")
     fun hello(): ResponseEntity<Void>{
         return ResponseEntity.ok().build()
     }
