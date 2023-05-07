aaa6c36e13881c74a548a9922836b7e9e6694875
이경창 <lkc263@naver.com>
2023-05-08 02:20:47 +0900

    [BE] Fix : 시설 TDD 파일 구조 수정

--- backend/src/test/kotlin/ssafy/autonomous/pathfinder/facility/service/FacilityServiceImplTest.kt
+++ backend/src/test/kotlin/ssafy/autonomous/pathfinder/facility/service/FacilityServiceImplTest.kt
@@ -1,5 +1,6 @@
 package ssafy.autonomous.pathfinder.facility.service
 
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.DisplayName
@@ -32,15 +33,22 @@ class FacilityServiceImplTest @Autowired constructor(
     @DisplayName("시설 타입 조회")
     fun  facilityDynamic(){
         // given
+        
+        val facilityList = mutableListOf<String>()
+        facilityList.add(FacilityTypesRequestDto("CLASSROOM401").filteringSearch)
+        facilityList.add(FacilityTypesRequestDto("CLASSROOM402").filteringSearch)
+        facilityList.add(FacilityTypesRequestDto("AUDITORIUM101").filteringSearch)
+
         val request = FacilityTypesRequestDto("CLASSROOM401")
 
         // when
         val passfinderResponse = facilityService.facilityDynamic(request)
-        val filterSearch: List<Facility> ?= passfinderResponse?.data
+        val filterSearch: List<Facility> = passfinderResponse
 
         // then
-        assertThat(passfinderResponse?.status).isEqualTo(HttpStatus.OK)
-        assertThat(filterSearch?.first(){ x -> x.getFacilityType() == "CLASSROOM401"})
+
+        assertThat(filterSearch.isNotEmpty())
+        assertThat(filterSearch.all { it.getFacilityType() == "CLASSROOM401" })
 
     }
 
