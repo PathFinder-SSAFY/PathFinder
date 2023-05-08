e20a1bc798bbe7884829f3f419a6e1fc54dca4b5
lkc263 <lkc263@naver.com>
2023-05-08 14:17:03 +0900

    [BE] Fix : WebConfigure 추가

---
+++ backend/src/main/kotlin/ssafy/autonomous/pathfinder/global/config/WebConfigure.kt
@@ -0,0 +1,21 @@
+package ssafy.autonomous.pathfinder.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfigure : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+    }
+}
+
+
+
