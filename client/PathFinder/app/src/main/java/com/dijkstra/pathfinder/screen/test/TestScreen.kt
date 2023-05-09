5cb27ddbc780d192eb18a1c6506214c67167052a
bigyoung8375@naver.com <bigyoung8375@naver.com>
2023-05-09 17:50:51 +0900

    [FE] Feat SharedFlow & StateFlow Retrofit Network Developing

--- client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/screen/test/TestScreen.kt
+++ client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/screen/test/TestScreen.kt
@@ -1,13 +1,10 @@
 package com.dijkstra.pathfinder.screen
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.ExperimentalFoundationApi
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,8 +14,9 @@ import com.dijkstra.pathfinder.screen.test.TestViewModel
 import com.dijkstra.pathfinder.util.NetworkResult
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+
+private const val TAG = "TestScreen_싸피"
 
 @Composable
 fun TestScreen(
@@ -26,17 +24,21 @@ fun TestScreen(
     testViewModel: TestViewModel = hiltViewModel()
 ) {
     // val testCallState = testViewModel.testCallStateFlow.collectAsState()
-    val testCall2State =
-        testViewModel.testCall2SharedFlow.collectAsState(NetworkResult.Loading())
+    // val testCall2State = testViewModel.testCall2SharedFlow.collectAsState(NetworkResult.Loading())
 
     val response = testViewModel.testCall2ResponseFlow.value
+    var responseResultData by remember {
+        mutableStateOf<NetworkResult<Int>>(NetworkResult.Loading())
+    }
 
-//    TestContent(data = "") {
-//        CoroutineScope(Dispatchers.IO).launch {
-//            testViewModel.testCall2()
-//        }
-//    }
+    val testCall2StateFlowResponse = testViewModel.testCall2StateFlow.collectAsState()
 
+    val collectAs = testViewModel.testCall2SharedFlow.collectAsState(NetworkResult.Loading())
+    collectAs.value.let {
+        // 이거 안됌
+        Log.d(TAG, "collectAs: $it")
+        responseResultData = it
+    }
 
     Surface(
         modifier = Modifier.fillMaxSize()
@@ -50,17 +52,17 @@ fun TestScreen(
             androidx.compose.material3.Button(onClick = {
                 CoroutineScope(Dispatchers.IO).launch {
                     // testViewModel.testCall()
+                    testViewModel.testCall2()
                 }
-                testViewModel.testCall2()
             }) {
                 Text(text = "Test Call Button")
             }
 
-            response.let {
+            collectAs.value.let {
                 when (it) {
                     is NetworkResult.Success -> {
                         if (it.data == 200) {
-                            Text(text = "Success")
+                            Text(text = "collectAs : Success")
                         }
                     }
                     is NetworkResult.Error -> {
@@ -71,6 +73,60 @@ fun TestScreen(
                     }
                 }
             }
+
+            Spacer(Modifier.padding(40.dp))
+
+            Text(text = "아래는 SharedFlow결과값을 관찰해서 mutableState responseResultData로 값을 전달.")
+
+            responseResultData.let {
+                when (it) {
+                    is NetworkResult.Success -> {
+                        if (it.data == 200) {
+                            Text(text = "responseResultData : Success")
+                        }
+                    }
+                    is NetworkResult.Error -> {
+                        Text(text = "Test Call Error")
+                    }
+                    is NetworkResult.Loading -> {
+                        Text(text = "Test Call Loading..")
+                    }
+                }
+            }
+
+            Spacer(Modifier.padding(40.dp))
+            Text(text = "아래는 StateFlow.let으로 짜여진 코드")
+            testCall2StateFlowResponse.let {
+                when (it.value) {
+                    is NetworkResult.Success -> {
+                        if (it.value.data == 200) {
+                            Text(text = "testCall2StateFlowResponse : Success")
+                        }
+                    }
+                    is NetworkResult.Error -> {
+                        Text(text = "Test Call Error")
+                    }
+                    is NetworkResult.Loading -> {
+                        Text(text = "Test Call Loading..")
+                    }
+                }
+            }
+
+//            response.let {
+//                when (it) {
+//                    is NetworkResult.Success -> {
+//                        if (it.data == 200) {
+//                            Text(text = "Success")
+//                        }
+//                    }
+//                    is NetworkResult.Error -> {
+//                        Text(text = "Test Call Error")
+//                    }
+//                    is NetworkResult.Loading -> {
+//                        Text(text = "Test Call Loading..")
+//                    }
+//                }
+//            }
 
 //            testCall2State.value.let {
 //                when (it) {
@@ -109,58 +165,4 @@ fun TestScreen(
 
 } // End of TestScreen
 
-@Composable
-fun TestContent(data: String, onTest: () -> Unit) {
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "TEST SCREEN")
-            Spacer(modifier = Modifier.padding(30.dp))
-            androidx.compose.material3.Button(onClick = {
-                onTest()
-            }) {
-                Text(text = "Test Call Button")
-            }
-
-
-//            testCall2State.value.let {
-//                when (it) {
-//                    is NetworkResult.Success -> {
-//                        if (it.data == 200) {
-//                            Text(text = "Test Call Success")
-//                        }
-//                    }
-//                    is NetworkResult.Error -> {
-//                        Text(text = "Test Call Error")
-//                    }
-//                    is NetworkResult.Loading -> {
-//                        Text(text = "Test Call Loading..")
-//                    }
-//                }
-//            }
-
-//            testCallState.value.let {
-//                // LaunchedEffect 써야되나?? -> 안써도 되는듯
-//                when (it) {
-//                    is NetworkResult.Success -> {
-//                        if (it.data == 200) {
-//                            Text(text = "Test Call Success")
-//                        }
-//                    }
-//                    is NetworkResult.Error -> {
-//                        Text(text = "Test Call Error")
-//                    }
-//                    is NetworkResult.Loading -> {
-//                        Text(text = "Test Call Loading..")
-//                    }
-//                }
-//            }
-        }
-    }
-
-} // End of TestContent
 
--- client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/screen/test/TestViewModel.kt
+++ client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/screen/test/TestViewModel.kt
@@ -24,26 +24,51 @@ class TestViewModel @Inject constructor(
 
     // ================================ LiveData 코드 Flow코드로 수정
 
+    // MutableSharedFlow
     private val _testCall2SharedFlow = MutableSharedFlow<NetworkResult<Int>>()
     var testCall2SharedFlow = _testCall2SharedFlow.asSharedFlow()
         private set
 
+    // mutableStateOf
     // Default State = NetworkResult.Loading()
     private val _testCall2ResponseFlow = mutableStateOf<NetworkResult<Int>>(NetworkResult.Loading())
     var testCall2ResponseFlow: State<NetworkResult<Int>> = _testCall2ResponseFlow
         private set
 
-    fun testCall2() {
+    // Default State = NetworkResult.Loading()
+    private val _testCall2StateFlow = MutableStateFlow<NetworkResult<Int>>(NetworkResult.Loading())
+    var testCall2StateFlow: StateFlow<NetworkResult<Int>> = _testCall2StateFlow
+        private set
+
+    /*
+        testCall().onStart를 통해서 NetworkResult.Loading()으로 시작
+
+        onStart함수는 Flow가 실행 될 때 특정데이터를 방출할 수 있도록 지원한다.
+        선언 위치는 사용전이면 상관없으며 여러 번 사용하면 순서대로 전부 실행된다.
+     */
+
+    suspend fun testCall2() {
         viewModelScope.launch {
             testRepository.testCall2().onStart {
+                _testCall2ResponseFlow.value = NetworkResult.Loading()
+                _testCall2StateFlow.emit(NetworkResult.Loading())
                 _testCall2SharedFlow.emit(NetworkResult.Loading())
             }.catch {
                 it.printStackTrace()
                 _testCall2ResponseFlow.value = NetworkResult.Error(
                     it.message ?: "Something went wrong!"
                 )
+                _testCall2StateFlow.emit(
+                    NetworkResult.Error(
+                        it.message ?: "Something went wrong!"
+                    )
+                )
             }.collect {
                 _testCall2ResponseFlow.value = NetworkResult.Success(it.code())
+                _testCall2StateFlow.emit(
+                    NetworkResult.Success(it.code())
+                )
+                _testCall2SharedFlow.emit(NetworkResult.Success(it.code()))
             }
         }
     } // End of testCall2
