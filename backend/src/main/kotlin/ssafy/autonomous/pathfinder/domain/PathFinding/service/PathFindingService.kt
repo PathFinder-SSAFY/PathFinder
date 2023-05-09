6ec292b76c3469647c1f8265e3115b5028528be2
고청천 <g2c1010@naver.com>
2023-05-09 11:01:17 +0900

    [BE] Feat: application-local-ko.yml 추가

--- backend/src/main/kotlin/ssafy/autonomous/pathfinder/domain/PathFinding/service/PathFindingService.kt
+++ backend/src/main/kotlin/ssafy/autonomous/pathfinder/domain/PathFinding/service/PathFindingService.kt
@@ -1,7 +1,7 @@
 package ssafy.autonomous.pathfinder.domain.PathFinding.service
 
-import ssafy.autonomous.pathfinder.domain.PathFinding.dto.NodeDto
+import ssafy.autonomous.pathfinder.domain.PathFinding.dto.Node
 
 interface PathFindingService {
-    fun findPath(start: NodeDto, goal: NodeDto, obstacles: List<NodeDto>): List<NodeDto>?
+    fun findPath(start: Node, goal: Node, obstacles: List<Node>): List<Node>?
 }
