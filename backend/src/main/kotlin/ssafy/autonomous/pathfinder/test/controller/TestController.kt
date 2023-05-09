839800dbd3a7ca8e5bd567b99c3d77c7d2f6eaf2
고청천 <g2c1010@naver.com>
2023-05-09 21:47:53 +0900

    [BE] Feat: fail API 작성, 통신코드 for 400에러 상황 테스트

--- backend/src/main/kotlin/ssafy/autonomous/pathfinder/test/controller/TestController.kt
+++ backend/src/main/kotlin/ssafy/autonomous/pathfinder/test/controller/TestController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @Api(tags=["통신 테스트 컨트롤러"])
 @Controller
@@ -14,5 +15,10 @@ class TestController {
     @GetMapping("/hello")
     fun hello(): ResponseEntity<Void>{
         return ResponseEntity.ok().build()
+    }
+
+    @GetMapping("/fail")
+    fun badRequest(): ResponseEntity<Void> {
+        return ResponseEntity.badRequest().build()
     }
 }
