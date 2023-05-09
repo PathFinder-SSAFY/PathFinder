12ae9ac916c011af6185090c1162bc32b1f4487e
이경창 <lkc263@naver.com>
2023-05-09 22:59:41 +0900

    [BE] Fix : ResponseDto 수정

--- backend/src/main/kotlin/ssafy/autonomous/pathfinder/global/common/response/ApiResponse.kt
+++ backend/src/main/kotlin/ssafy/autonomous/pathfinder/global/common/response/ApiResponse.kt
@@ -1,12 +1,9 @@
 package ssafy.autonomous.pathfinder.global.common.response
 
 import org.springframework.http.HttpStatus
-import ssafy.autonomous.pathfinder.global.common.ResultType
 
 
 data class ApiResponse(
-    val result: ResultType = ResultType.SUCCESS,
-    val statusCode: Int = HttpStatus.OK.value(),
-    val message: String = "성공했습니다.",
+//    val status: Int = HttpStatus.OK.value(),
     val data: Any?
 )
--- backend/src/main/kotlin/ssafy/autonomous/pathfinder/global/common/response/ErrorResponse.kt
+++ backend/src/main/kotlin/ssafy/autonomous/pathfinder/global/common/response/ErrorResponse.kt
@@ -8,10 +8,9 @@ import java.time.LocalDateTime
 
 data class ErrorResponse(
     // 에러 발생시간, 상태, 메세지, 에러 로그
-    val timestamp: LocalDateTime = LocalDateTime.now(),
-    val status: Int,
-    val message: String?,
-    val errors: List<FieldError>
+//    val timestamp: LocalDateTime = LocalDateTime.now(),
+//    val status: Int,
+    val data: String
 ) {
 
     companion object {
@@ -21,41 +20,18 @@ data class ErrorResponse(
         }
 
         fun toResponseEntity(result: BindingResult?): ResponseEntity<ErrorResponse?> {
-            return ResponseEntity.badRequest().body(result?.let { ErrorResponse(it) })
-        }
-
-
-        data class FieldError(
-            val field: String,
-            val value: String?,
-            val reason: String?
-        ) {
-
-            companion object {
-                fun of(bindingResult: BindingResult): List<FieldError> {
-                    val fieldErrors = bindingResult.fieldErrors
-                    return fieldErrors.map { error ->
-                        FieldError(
-                            error.field,
-                            error.rejectedValue?.toString(),
-                            if (error.code == "typeMismatch") "입력 값에 대한 예외입니다." else error.defaultMessage
-                        )
-                    }
-                }
-            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result?.let { ErrorResponse(it) })
         }
     }
 
     // 생성자
     constructor(errorCode: ErrorCode) : this(
-        status = errorCode.status.value(),
-        message = errorCode.message,
-        errors = emptyList()
+//        status = errorCode.status.value(),
+        data = errorCode.message,
     )
 
     constructor(result: BindingResult) : this(
-        status = HttpStatus.BAD_REQUEST.value(),
-        message = "입력 조건에 대한 예외이다.",
-        errors = FieldError.of(result)
+//        status = HttpStatus.BAD_REQUEST.value(),
+        data = result.fieldErrors.map { error -> error.defaultMessage}.joinToString { ", " } // 에러를 map 형식으로 저장
     )
 }
