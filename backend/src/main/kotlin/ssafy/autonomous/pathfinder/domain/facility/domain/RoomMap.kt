2520f1a9192b3b37108ff4d3428fc56653df5197
lkc263 <lkc263@naver.com>
2023-05-10 17:38:03 +0900

    [BE] Fix : 방 정보 Entity 수정

--- backend/src/main/kotlin/ssafy/autonomous/pathfinder/domain/facility/domain/RoomMap.kt
+++ backend/src/main/kotlin/ssafy/autonomous/pathfinder/domain/facility/domain/RoomMap.kt
@@ -5,9 +5,16 @@ import javax.persistence.*
 @Entity
 class RoomMap(
 
-        // 위도, 경도
+        // Room 중앙 위치
         val longitude: Double,
         val latitude: Double,
+
+
+        // 입구 좌상, 우상, 좌하, 우하 추가
+        val entrancLeftUp: Double ?= 0.0,
+        val entranceRightUp: Double ?= 0.0,
+        val entranceLeftDown: Double ?= 0.0,
+        val entranceRightDown: Double ?= 0.0,
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,8 +28,8 @@ class RoomMap(
         var floors: Floors ?= null,
 
         // 일대일 상하좌우 방향
-        @OneToOne(fetch = FetchType.LAZY, mappedBy="roomMap", cascade = [CascadeType.ALL])
-        var direction: Direction ?= null,
+//        @OneToOne(fetch = FetchType.LAZY, mappedBy="roomMap", cascade = [CascadeType.ALL])
+//        var direction: Direction ?= null,
 
         // 일대일 weight
         @OneToOne(fetch = FetchType.LAZY, mappedBy="roomMap", cascade = [CascadeType.ALL])
