48ef5f0873ec4bb8bed8c0ea00c2c19cf0176412
이경창 <lkc263@naver.com>
2023-05-13 14:25:35 +0900

    [BE] Feat : NFC id DTO 추가

--- backend/src/main/kotlin/ssafy/autonomous/pathfinder/domain/building/controller/BuildingController.kt
+++ backend/src/main/kotlin/ssafy/autonomous/pathfinder/domain/building/controller/BuildingController.kt
@@ -4,9 +4,8 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ssafy.autonomous.pathfinder.domain.building.dto.request.BuildingNfcRequestDto
 import ssafy.autonomous.pathfinder.domain.building.service.BuildingService
@@ -19,8 +18,8 @@ class BuildingController(
 ) {
 
     @ApiOperation(value = "초기 값, nfc")
-    @GetMapping("/building/nfc")
-    fun getBuildingNfc(): ResponseEntity<ApiResponse>{
+    @PostMapping("/building/nfc")
+    fun getBuildingNfc(@RequestBody buildingNfcRequestDto: BuildingNfcRequestDto): ResponseEntity<ApiResponse>{
         return ResponseEntity.status(HttpStatus.OK).body(
             ApiResponse(
                 data = buildingService.getBuildingNfc()
