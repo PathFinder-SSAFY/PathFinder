c55cbbe2e8ebba0b66b03ed5626e52eab8ced85c
Kihun SONG <kihunbuzz@naver.com>
2023-05-17 10:54:49 +0900

    [FE] Delete : Unused Files

--- client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/data/dto/ExampleDto.kt
+++
@@ -1,6 +0,0 @@
-package com.dijkstra.pathfinder.data.dto
-
-data class ExampleDto(
-    val id: Int,
-    val name: String
-)
--- client/PathFinder/app/src/main/java/com/dijkstra/pathfinder/data/dto/SearchTest.kt
+++
@@ -1,9 +0,0 @@
-package com.dijkstra.pathfinder.data.dto
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class SearchTest @JvmOverloads constructor(
-    var filteringSearch: String? = ""
-) : Parcelable
